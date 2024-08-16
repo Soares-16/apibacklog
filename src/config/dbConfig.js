d98b6f88ef06af0f87048565a55a497b109843a7
Sandrinha Sentinela <92555862+Soares-16@users.noreply.github.com>
2024-08-16 18:42:00 -0300

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize"; 
 
-const sequelize = new Sequelize('back]
+const sequelize = new Sequelize('backlog', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
 sequelize.authenticate()
     .then(() => {
-        console.log('Conectado ao MySQL com Sequel')
-
+        console.log('Conectado ao MySQL com Sequelize');
     })
+    .catch(err => {
+        .console.error('Não foi possivel conectar ao banco de doados!' , err);
+    });
